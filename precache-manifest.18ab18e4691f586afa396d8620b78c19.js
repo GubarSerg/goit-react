f19f143b5741f5e37f3d6acce92a3598c50af7d2
GubarSerg <gubar.serg@gmail.com>
2019-05-28 00:03:15 +0300

    Updates

---
+++ precache-manifest.18ab18e4691f586afa396d8620b78c19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ec6f0d552a56b63074a4e245313c05",
+    "url": "/goit-react_HW-01/index.html"
+  },
+  {
+    "revision": "edd0a38a6c0c96ddd211",
+    "url": "/goit-react_HW-01/static/css/main.f7cfbf55.chunk.css"
+  },
+  {
+    "revision": "294a3c23aa9869ae810b",
+    "url": "/goit-react_HW-01/static/js/2.7272bfd8.chunk.js"
+  },
+  {
+    "revision": "edd0a38a6c0c96ddd211",
+    "url": "/goit-react_HW-01/static/js/main.e01a9e85.chunk.js"
+  },
+  {
+    "revision": "b60ace877c4effbe74fb",
+    "url": "/goit-react_HW-01/static/js/runtime~main.3205e6d9.js"
+  }
+]);
